fbef15571fe5a3e59b411c4829e05a81930363c3
Dina <16936@rpl.smkn1bawang.sch.id>
2022-07-11 09:46:30 +0700

    initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const { get } = require("http");
+
+let express = required('express');
+
+let app = express();
+
+app.use(express.static(__dirname+'/dist/wisata-jateng'));
+
+app.get('/*', (req,resp)=>{
+    resp.sendFile(__dirname+'/dist/wisata-jateng/index.html');
+});
+
+app.listen(process.env.PORT || 8080);
