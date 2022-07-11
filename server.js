134be33648be01b2a5ecd1a4680f7ec047d6c68b
Dina <16936@rpl.smkn1bawang.sch.id>
2022-07-11 10:33:38 +0700

    website-angular

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
-const { get } = require("http");
-
-let express = required('express');
+let express = require('express');
 
 let app = express();
 
 app.use(express.static(__dirname+'/dist/wisata-jateng'));
 
-app.get('/*', (req,resp)=>{
+app.get('/*', (req, resp)=>{
     resp.sendFile(__dirname+'/dist/wisata-jateng/index.html');
 });
+
 
 app.listen(process.env.PORT || 8080);
